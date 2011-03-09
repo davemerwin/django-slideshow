4d89ea6aae219b090c8be1163b77660a71eed939
Dave Merwin <dmerwin@orcasinc.com>
2011-03-08 17:08:40 -0800

    updating teh imagespecs

--- slideshow/imagespecs.py
+++ slideshow/imagespecs.py
@@ -1,4 +1,5 @@
 # slideshow/imagespecs.py
+# Sizes are up to you. I've done common size based on 960.gs 16 column view
 
 from imagekit.specs import ImageSpec 
 from imagekit import processors 
@@ -9,9 +10,30 @@ class ResizeThumb(processors.Resize):
     height = 102 
     crop_horz_field = 1
     crop_vert_field = 1
+    
+# first we define our small resize processor 
+class ResizeSmall(processors.Resize): 
+    width = 100
+    height = 100 
+    crop_horz_field = 1
+    crop_vert_field = 1
 
-# now we define a display size resize processor
-class ResizeDisplay(processors.Resize):
+# first we define our medium resize processor 
+class ResizeMedium(processors.Resize): 
+    width = 136
+    height = 102 
+    crop_horz_field = 1
+    crop_vert_field = 1
+
+# first we define our large resize processor 
+class ResizeLarge(processors.Resize): 
+    width = 470
+    height = 102 
+    crop_horz_field = 1
+    crop_vert_field = 1
+
+# now we define a extra large size resize processor
+class ResizeExtraLarge(processors.Resize):
     width = 940
     height = 300
     crop = True
@@ -25,13 +47,30 @@ class EnchanceThumb(processors.Adjustment):
 
 # now we can define our thumbnail spec 
 class Thumbnail(ImageSpec): 
-    access_as = 'thumbnail_image' 
+    access_as = 'thumbnail' 
     pre_cache = True
-    processors = [ResizeThumb, EnchanceThumb] 
+    processors = [ResizeThumb, EnchanceThumb]
+    
+# now we can define our Small spec 
+class Small(ImageSpec): 
+    access_as = 'small' 
+    pre_cache = True
+    processors = [ResizeSmall]
+    
+# now we can define our Medium spec 
+class Medium(ImageSpec): 
+    access_as = 'medium' 
+    pre_cache = True
+    processors = [ResizeMedium]
+    
+# now we can define our Large spec 
+class Large(ImageSpec): 
+    access_as = 'medium' 
+    pre_cache = True
+    processors = [ResizeLarge]
 
-# and our display spec
-class Display(ImageSpec):
-    access_as = 'display_image'
+# and our ExtraLarge spec
+class ExtraLarge(ImageSpec):
+    access_as = 'extralarge'
     pre_cache = True
-    increment_count = True
-    processors = [ResizeDisplay]
+    processors = [ResizeExtraLarge]
